a5692354427ac66fe76267dab3f35a75aa43c6ab
riteekagawand <riteekagawand7@gmail.com>
2025-04-02 00:06:46 +0530

    Removed Google Translate

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom"; // Outlet renders nested routes
-import GoogleTranslate from "./snippet/GoogleTranslate";
+// import GoogleTranslate from "./snippet/GoogleTranslate";
 
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <GoogleTranslate />
+      {/* <GoogleTranslate /> */}
       {/* Add common UI like navbar or sidebar here if needed */}
       <main className="flex-grow">
         <Outlet /> {/* Renders the nested route components */}
